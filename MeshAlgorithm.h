95c66a0de81d5afc99afd32c33c443654b5a388c
itsuhane <itsuhane@users.noreply.github.com>
2018-01-06 23:06:23 +0800

    Add files via upload

--- MeshAlgorithm.h
+++ MeshAlgorithm.h
@@ -4,8 +4,8 @@
 #include <OpenMesh/Core/Utils/Property.hh>
 
 /*
-    for each (vertex_handle in mesh.valid_vertices) {
-        fcn(mesh, vertex_handle);
+    for each (vertex in mesh.valid_vertices) {
+        fcn(mesh, vertex);
     }
 */
 template <typename Mesh, typename Fn>
@@ -16,8 +16,8 @@ inline void ForEachVertex(Mesh &mesh, Fn fcn) {
 }
 
 /*
-    for each (face_handle in mesh.valid_faces) {
-        fcn(mesh, face_handle);
+    for each (face in mesh.valid_faces) {
+        fcn(mesh, face);
     }
 */
 template <typename Mesh, typename Fn>
@@ -28,8 +28,8 @@ inline void ForEachFace(Mesh &mesh, Fn fcn) {
 }
 
 /*
-    for each (face_handle incident to mesh.vertex(vh) in CCW-order) {
-        fcn(mesh, face_handle);
+    for each (face incident to mesh.vertex(vh) in CCW-order) {
+        fcn(mesh, face);
     }
 */
 template <typename Mesh, typename Fn>
@@ -40,8 +40,8 @@ inline void ForEachVertexFaceCCW(Mesh &mesh, const typename Mesh::VertexHandle &
 }
 
 /*
-    for each (face_handle incident to mesh.vertex(vh) in CW-order) {
-        fcn(mesh, face_handle);
+    for each (face incident to mesh.vertex(vh) in CW-order) {
+        fcn(mesh, face);
     }
 */
 template <typename Mesh, typename Fn>
@@ -52,8 +52,8 @@ inline void ForEachVertexFaceCW(Mesh &mesh, const typename Mesh::VertexHandle &v
 }
 
 /*
-    for each (vertex_handle incident to mesh.vertex(vh) in CCW-order) {
-        fcn(mesh, vertex_handle);
+    for each (vertex incident to mesh.vertex(vh) in CCW-order) {
+        fcn(mesh, vertex);
     }
 */
 template <typename Mesh, typename Fn>
@@ -64,8 +64,8 @@ inline void ForEachVertexVertexCCW(Mesh &mesh, const typename Mesh::VertexHandle
 }
 
 /*
-    for each (vertex_handle incident to mesh.vertex(vh) in CW-order) {
-        fcn(mesh, vertex_handle);
+    for each (vertex incident to mesh.vertex(vh) in CW-order) {
+        fcn(mesh, vertex);
     }
 */
 template <typename Mesh, typename Fn>
@@ -76,8 +76,8 @@ inline void ForEachVertexVertexCW(Mesh &mesh, const typename Mesh::VertexHandle 
 }
 
 /*
-    for each (vertex_handle incident to mesh.face(fh) in CCW-order) {
-        fcn(mesh, vertex_handle);
+    for each (vertex incident to mesh.face(fh) in CCW-order) {
+        fcn(mesh, vertex);
     }
 */
 template <typename Mesh, typename Fn>
@@ -88,8 +88,8 @@ inline void ForEachFaceVertexCCW(Mesh &mesh, const typename Mesh::FaceHandle &fh
 }
 
 /*
-    for each (vertex_handle incident to mesh.face(fh) in CW-order) {
-        fcn(mesh, vertex_handle);
+    for each (vertex incident to mesh.face(fh) in CW-order) {
+        fcn(mesh, vertex);
     }
 */
 template <typename Mesh, typename Fn>
@@ -100,8 +100,8 @@ inline void ForEachFaceVertexCW(Mesh &mesh, const typename Mesh::FaceHandle &fh,
 }
 
 /*
-    for each (face_handle incident to mesh.face(fh) in CCW-order) {
-        fcn(mesh, face_handle);
+    for each (face incident to mesh.face(fh) in CCW-order) {
+        fcn(mesh, face);
     }
 */
 template <typename Mesh, typename Fn>
@@ -112,8 +112,8 @@ inline void ForEachFaceFaceCCW(Mesh &mesh, const typename Mesh::FaceHandle &fh, 
 }
 
 /*
-    for each (face_handle incident to mesh.face(fh) in CW-order) {
-        fcn(mesh, face_handle);
+    for each (face incident to mesh.face(fh) in CW-order) {
+        fcn(mesh, face);
     }
 */
 template <typename Mesh, typename Fn>
@@ -124,26 +124,14 @@ inline void ForEachFaceFaceCW(Mesh &mesh, const typename Mesh::FaceHandle &fh, F
 }
 
 /*
-    for each (vertex_handle in mesh.valid_vertices) {
-        mesh.vph[vertex_handle] = val;
+    for each (vertex in mesh.valid_vertices) {
+        mesh.vph[vertex] = val;
     }
 */
 template <typename Mesh, typename T>
-inline void FillVertexPropertyTo(Mesh &mesh, OpenMesh::VPropHandleT<T> vph, const T &val) {
+inline void FillVertexPropertyTo(Mesh &mesh, const OpenMesh::VPropHandleT<T> &vph, const T &val) {
     ForEachVertex(mesh, [&vph, &val](Mesh &mesh, const Mesh::VertexHandle &vh) {
         mesh.property(vph, vh) = val;
-    });
-}
-
-/*
-    for each (face_handle in mesh.valid_faces) {
-        mesh.fph[face_handle] = val;
-    }
-*/
-template <typename Mesh, typename T>
-inline void FillFacePropertyTo(Mesh &mesh, OpenMesh::FPropHandleT<T> fph, const T &val) {
-    ForEachFace(mesh, [&fph, &val](Mesh &mesh, const Mesh::FaceHandle &fh) {
-        mesh.property(fph, fh) = val;
     });
 }
 
@@ -159,6 +147,18 @@ inline OpenMesh::VPropHandleT<T> FillVertexProperty(Mesh &mesh, const T &val) {
 }
 
 /*
+    for each (face in mesh.valid_faces) {
+        mesh.fph[face] = val;
+    }
+*/
+template <typename Mesh, typename T>
+inline void FillFacePropertyTo(Mesh &mesh, const OpenMesh::FPropHandleT<T> &fph, const T &val) {
+    ForEachFace(mesh, [&fph, &val](Mesh &mesh, const Mesh::FaceHandle &fh) {
+        mesh.property(fph, fh) = val;
+    });
+}
+
+/*
     Create new face property and FillFacePropertyTo it.
 */
 template <typename Mesh, typename T>
@@ -170,26 +170,14 @@ inline OpenMesh::FPropHandleT<T> FillFaceProperty(Mesh &mesh, const T &val) {
 }
 
 /*
-    for each (vertex_handle in mesh.valid_vertices) {
-        mesh.vph[vertex_handle] = gen();
+    for each (vertex in mesh.valid_vertices) {
+        mesh.vph[vertex] = gen();
     }
 */
 template <typename Mesh, typename T, typename Fn>
-inline void GenerateVertexPropertyTo(Mesh &mesh, OpenMesh::VPropHandleT<T> vph, Fn gen) {
+inline void GenerateVertexPropertyTo(Mesh &mesh, const OpenMesh::VPropHandleT<T> &vph, Fn gen) {
     ForEachVertex(mesh, [&vph, &gen](Mesh &mesh, const Mesh::VertexHandle &vh) {
         mesh.property(vph, vh) = gen();
-    });
-}
-
-/*
-    for each (face_handle in mesh.valid_faces) {
-        mesh.fph[face_handle] = gen();
-    }
-*/
-template <typename Mesh, typename T, typename Fn>
-inline void GenerateFacePropertyTo(Mesh &mesh, OpenMesh::FPropHandleT<T> fph, Fn gen) {
-    ForEachFace(mesh, [&fph, &gen](Mesh &mesh, const Mesh::FaceHandle &fh) {
-        mesh.property(fph, fh) = gen();
     });
 }
 
@@ -205,6 +193,18 @@ inline auto GenerateVertexProperty(Mesh &mesh, Fn gen) -> OpenMesh::VPropHandleT
 }
 
 /*
+    for each (face in mesh.valid_faces) {
+        mesh.fph[face] = gen();
+    }
+*/
+template <typename Mesh, typename T, typename Fn>
+inline void GenerateFacePropertyTo(Mesh &mesh, const OpenMesh::FPropHandleT<T> &fph, Fn gen) {
+    ForEachFace(mesh, [&fph, &gen](Mesh &mesh, const Mesh::FaceHandle &fh) {
+        mesh.property(fph, fh) = gen();
+    });
+}
+
+/*
     Create new face property and GenerateFacePropertyTo it.
 */
 template <typename Mesh, typename Fn>
@@ -216,12 +216,12 @@ inline auto GenerateFaceProperty(Mesh &mesh, Fn gen) -> OpenMesh::FPropHandleT<t
 }
 
 /*
-    for each (vertex_handle in mesh.valid_vertices) {
-        mesh.vph_to[vertex_handle] = tfm(mesh.vph_from[vertex_handle]);
+    for each (vertex in mesh.valid_vertices) {
+        mesh.vph_to[vertex] = tfm(mesh.vph_from[vertex]);
     }
 */
 template <typename Mesh, typename F, typename T, typename Fn>
-inline void MapVertexPropertyTo(Mesh &mesh, OpenMesh::VPropHandleT<F> vph_from, OpenMesh::VPropHandleT<T> vph_to, Fn tfm) {
+inline void MapVertexPropertyTo(Mesh &mesh, const OpenMesh::VPropHandleT<F> &vph_from, const OpenMesh::VPropHandleT<T> &vph_to, Fn tfm) {
     ForEachVertex(mesh, [&vph_from, &vph_to, &tfm](Mesh &mesh, const Mesh::VertexHandle &vh) {
         mesh.property(vph_to, vh) = tfm(mesh.property(vph_from, vh));
     });
@@ -231,7 +231,7 @@ inline void MapVertexPropertyTo(Mesh &mesh, OpenMesh::VPropHandleT<F> vph_from, 
     Create new vertex property and MapVertexPropertyTo it.
 */
 template <typename Mesh, typename F, typename Fn, typename T = std::decay<typename std::result_of<Fn(F)>::type>::type>
-inline OpenMesh::VPropHandleT<T> MapVertexProperty(Mesh &mesh, OpenMesh::VPropHandleT<F> vph, Fn tfm) {
+inline OpenMesh::VPropHandleT<T> MapVertexProperty(Mesh &mesh, const OpenMesh::VPropHandleT<F> &vph, Fn tfm) {
     OpenMesh::VPropHandleT<T> vph_to;
     mesh.add_property(vph_to);
     MapVertexPropertyTo(mesh, vph, vph_to, tfm);
@@ -239,12 +239,12 @@ inline OpenMesh::VPropHandleT<T> MapVertexProperty(Mesh &mesh, OpenMesh::VPropHa
 }
 
 /*
-    for each (face_handle in mesh.valid_faces) {
-        mesh.fph_to[face_handle] = tfm(mesh.fph_from[face_handle]);
+    for each (face in mesh.valid_faces) {
+        mesh.fph_to[face] = tfm(mesh.fph_from[face]);
     }
 */
 template <typename Mesh, typename F, typename T, typename Fn>
-inline void MapFacePropertyTo(Mesh &mesh, OpenMesh::FPropHandleT<F> fph_from, OpenMesh::FPropHandleT<T> fph_to, Fn tfm) {
+inline void MapFacePropertyTo(Mesh &mesh, const OpenMesh::FPropHandleT<F> &fph_from, const OpenMesh::FPropHandleT<T> &fph_to, Fn tfm) {
     ForEachFace(mesh, [&fph_from, &fph_to, &tfm](Mesh &mesh, const Mesh::FaceHandle &fh) {
         mesh.property(fph_to, fh) = tfm(mesh.property(fph_from, fh));
     });
@@ -254,7 +254,7 @@ inline void MapFacePropertyTo(Mesh &mesh, OpenMesh::FPropHandleT<F> fph_from, Op
     Create new face property and MapFacePropertyTo it.
 */
 template <typename Mesh, typename F, typename Fn, typename T = std::decay<typename std::result_of<Fn(F)>::type>::type>
-inline OpenMesh::FPropHandleT<T> MapFaceProperty(Mesh &mesh, OpenMesh::FPropHandleT<F> fph, Fn tfm) {
+inline OpenMesh::FPropHandleT<T> MapFaceProperty(Mesh &mesh, const OpenMesh::FPropHandleT<F> &fph, Fn tfm) {
     OpenMesh::FPropHandleT<T> fph_to;
     mesh.add_property(fph_to);
     MapVertexPropertyTo(mesh, fph, fph_to, tfm);
@@ -263,12 +263,12 @@ inline OpenMesh::FPropHandleT<T> MapFaceProperty(Mesh &mesh, OpenMesh::FPropHand
 
 /*
     result = init;
-    for each (vertex_handle in mesh.valid_vertices) {
-        result = op(result, mesh.vph[vertex_handle]);
+    for each (vertex in mesh.valid_vertices) {
+        result = op(result, mesh.vph[vertex]);
     }
 */
 template <typename Mesh, typename T, typename Fn, typename R = T>
-inline R ReduceVertexProperty(Mesh &mesh, OpenMesh::VPropHandleT<T> vph, R init, Fn op) {
+inline R ReduceVertexProperty(Mesh &mesh, const OpenMesh::VPropHandleT<T> &vph, R init, Fn op) {
     ForEachVertex(mesh, [&init, &vph, &op](Mesh &mesh, const Mesh::VertexHandle &vh) {
         init = op(init, mesh.property(vph, vh));
     });
@@ -277,20 +277,28 @@ inline R ReduceVertexProperty(Mesh &mesh, OpenMesh::VPropHandleT<T> vph, R init,
 
 /*
     result = init;
-    for each (face_handle in mesh.valid_faces) {
-        result = op(result, mesh.fph[face_handle]);
+    for each (face in mesh.valid_faces) {
+        result = op(result, mesh.fph[face]);
     }
 */
 template <typename Mesh, typename T, typename Fn, typename R = T>
-inline R ReduceFaceProperty(Mesh &mesh, OpenMesh::FPropHandleT<T> fph, R init, Fn op) {
+inline R ReduceFaceProperty(Mesh &mesh, const OpenMesh::FPropHandleT<T> &fph, R init, Fn op) {
     ForEachFace(mesh, [&init, &fph, &op](Mesh &mesh, const Mesh::FaceHandle &vh) {
         init = op(init, mesh.property(fph, vh));
     });
     return init;
 }
 
-template <typename F, typename T, typename Fn>
-inline void ReduceVertexFacePropertyTo(Mesh &mesh, OpenMesh::FPropHandleT<F> fph_from, OpenMesh::VPropHandleT<T> vph_to, const T &init, Fn op) {
+/*
+    for each (vertex in mesh.valid_vertices) {
+        mesh.vph_to[vertex] = init;
+        for each (face incident to vertex) {
+            mesh.vph_to[vertex] = op(mesh.vph_to[vertex], mesh.fph_from[face]);
+        }
+    }
+*/
+template <typename Mesh, typename F, typename T, typename Fn>
+inline void ReduceVertexFacePropertyTo(Mesh &mesh, const OpenMesh::FPropHandleT<F> &fph_from, const OpenMesh::VPropHandleT<T> &vph_to, const T &init, Fn op) {
     ForEachVertex(mesh, [&fph_from, &vph_to, &init, &op](Mesh &mesh, const Mesh::VertexHandle &vh) {
         mesh.property(vph_to, vh) = init;
         ForEachVertexFaceCCW(mesh, vh, [&vh, &fph_from, &vph_to, &op](Mesh &mesh, const Mesh::FaceHandle &fh) {
@@ -299,8 +307,25 @@ inline void ReduceVertexFacePropertyTo(Mesh &mesh, OpenMesh::FPropHandleT<F> fph
     });
 }
 
-template <typename F, typename T, typename Fn>
-inline void ReduceVertexVertexPropertyTo(Mesh &mesh, OpenMesh::VPropHandleT<F> vph_from, OpenMesh::VPropHandleT<T> vph_to, const T &init, Fn op) {
+/* Create new vertex property and ReduceVertexFacePropertyTo it. */
+template <typename Mesh, typename F, typename T, typename Fn>
+inline OpenMesh::VPropHandleT<T> ReduceVertexFaceProperty(Mesh &mesh, const OpenMesh::FPropHandleT<F> &fph_from, const T &init, Fn op) {
+    OpenMesh::VPropHandleT<T> vph_to;
+    mesh.add_property(vph_to);
+    ReduceVertexFacePropertyTo(mesh, fph_from, vph_to, init, op);
+    return vph_to;
+}
+
+/*
+    for each (vertex in mesh.valid_vertices) {
+        mesh.vph_to[vertex] = init;
+        for each (neighbor_vertex incident to vertex) {
+            mesh.vph_to[vertex] = op(mesh.vph_to[vertex], mesh.vph_from[neighbor_vertex]);
+        }
+    }
+*/
+template <typename Mesh, typename F, typename T, typename Fn>
+inline void ReduceVertexVertexPropertyTo(Mesh &mesh, const OpenMesh::VPropHandleT<F> &vph_from, const OpenMesh::VPropHandleT<T> &vph_to, const T &init, Fn op) {
     ForEachVertex(mesh, [&vph_from, &vph_to, &init, &op](Mesh &mesh, const Mesh::VertexHandle &vh) {
         mesh.property(vph_to, vh) = init;
         ForEachVertexVertexCCW(mesh, vh, [&vh, &vph_from, &vph_to, &op](Mesh &mesh, const Mesh::VertexHandle &vh_from) {
@@ -309,8 +334,25 @@ inline void ReduceVertexVertexPropertyTo(Mesh &mesh, OpenMesh::VPropHandleT<F> v
     });
 }
 
-template <typename F, typename T, typename Fn>
-inline void ReduceFaceVertexPropertyTo(Mesh &mesh, OpenMesh::VPropHandleT<F> vph_from, OpenMesh::FPropHandleT<T> fph_to, const T &init, Fn op) {
+/* Create new vertex property and ReduceVertexVertexPropertyTo it.*/
+template <typename Mesh, typename F, typename T, typename Fn>
+inline OpenMesh::VPropHandleT<T> ReduceVertexVertexProperty(Mesh &mesh, const OpenMesh::VPropHandleT<F> &vph_from, const T &init, Fn op) {
+    OpenMesh::VPropHandleT<T> vph_to;
+    mesh.add_property(vph_to);
+    ReduceVertexVertexPropertyTo(mesh, vph_from, vph_to, init, op);
+    return vph_to;
+}
+
+/*
+    for each (face in mesh.valid_faces) {
+        mesh.fph_to[face] = init;
+        for each (vertex incident to face) {
+            mesh.fph_to[face] = op(mesh.fph_to[face], mesh.vph_from[vertex]);
+        }
+    }
+*/
+template <typename Mesh, typename F, typename T, typename Fn>
+inline void ReduceFaceVertexPropertyTo(Mesh &mesh, const OpenMesh::VPropHandleT<F> &vph_from, const OpenMesh::FPropHandleT<T> &fph_to, const T &init, Fn op) {
     ForEachFace(mesh, [&vph_from, &fph_to, &init, &op](Mesh &mesh, const Mesh::FaceHandle &fh) {
         mesh.property(fph_to, fh) = init;
         ForEachFaceVertexCCW(mesh, fh, [&fh, &vph_from, &fph_to, &op](Mesh &mesh, const Mesh::VertexHandle &vh) {
@@ -319,12 +361,38 @@ inline void ReduceFaceVertexPropertyTo(Mesh &mesh, OpenMesh::VPropHandleT<F> vph
     });
 }
 
-template <typename F, typename T, typename Fn>
-inline void ReduceFaceFacePropertyTo(Mesh &mesh, OpenMesh::VPropHandleT<F> fph_from, OpenMesh::FPropHandleT<T> fph_to, const T &init, Fn op) {
+/* Create new face property and ReduceFaceVertexPropertyTo it.*/
+template <typename Mesh, typename F, typename T, typename Fn>
+inline OpenMesh::VPropHandleT<T> ReduceFaceVertexProperty(Mesh &mesh, const OpenMesh::VPropHandleT<F> &vph_from, const T &init, Fn op) {
+    OpenMesh::FPropHandleT<T> fph_to;
+    mesh.add_property(fph_to);
+    ReduceFaceVertexPropertyTo(mesh, vph_from, fph_to, init, op);
+    return fph_to;
+}
+
+/*
+    for each (face in mesh.valid_faces) {
+        mesh.fph_to[face] = init;
+        for each (neighbor_face incident to face) {
+            mesh.fph_to[face] = op(mesh.fph_to[face], mesh.fph_from[neighbor_face]);
+        }
+    }
+*/
+template <typename Mesh, typename F, typename T, typename Fn>
+inline void ReduceFaceFacePropertyTo(Mesh &mesh, const OpenMesh::VPropHandleT<F> &fph_from, const OpenMesh::FPropHandleT<T> &fph_to, const T &init, Fn op) {
     ForEachFace(mesh, [&fph_from, &fph_to, &init, &op](Mesh &mesh, const Mesh::FaceHandle &fh) {
         mesh.property(fph_to, fh) = init;
         ForEachFaceFaceCCW(mesh, fh, [&fh, &fph_from, &fph_to, &op](Mesh &mesh, const Mesh::FaceHandle &fh_from) {
             mesh.property(fph_to, fh) = op(mesh.property(fph_to, fh), mesh.property(fph_from, fh_from));
         });
     });
+}
+
+/* Create new face property and ReduceFaceFacePropertyTo it.*/
+template <typename Mesh, typename F, typename T, typename Fn>
+inline OpenMesh::VPropHandleT<T> ReduceFaceVertexProperty(Mesh &mesh, const OpenMesh::FPropHandleT<F> &fph_from, const T &init, Fn op) {
+    OpenMesh::FPropHandleT<T> fph_to;
+    mesh.add_property(fph_to);
+    ReduceFaceFacePropertyTo(mesh, fph_from, fph_to, init, op);
+    return fph_to;
 }
